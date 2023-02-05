f424afbff3ada0eb86ff083a1df4a60b83ac7f11
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-02-05 12:41:52 -0300

    feat: atributes binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',  using selector by atributes
   //selector: '.app-servers', using selector by class
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  // template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    },2000)
+  }
 }
