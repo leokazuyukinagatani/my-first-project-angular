e3aae4ffef92114f0e99968018a58acd144ef1c5
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-02-05 11:25:19 -0300

    feat: any methods to selector an component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  // selector: '[app-servers]',  using selector by atributes
+  //selector: '.app-servers', using selector by class
+  selector: 'app-servers',
   template: `<app-server></app-server><app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
